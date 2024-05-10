e4811107ed1814595f6d9f66c728bbf6dc0f23ba
CSeungJoo <seungjoo13579@gmail.com>
2024-05-10 13:34:29 +0900

    init

---
+++ src/main/java/kr/pah/pcs/pcscoin/PcsCoinApplication.java
@@ -0,0 +1,13 @@
+package kr.pah.pcs.pcscoin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcsCoinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PcsCoinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/pah/pcs/pcscoin/PcsCoinApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.pah.pcs.pcscoin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcsCoinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
