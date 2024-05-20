ca9fb8e1f7d1c1ce8d22a56ce1273f66b170112c
CSeungJoo <seungjoo13579@gmail.com>
2024-05-20 18:43:20 +0900

    SHA256을 이용한 hashing 메서드 생성

--- src/main/java/kr/pah/pcs/pcscoin/global/common/SecurityUtils.java
+++ src/main/java/kr/pah/pcs/pcscoin/global/common/SecurityUtils.java
@@ -6,6 +6,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
 @Component
 @RequiredArgsConstructor
 public class SecurityUtils {
@@ -20,4 +25,15 @@ public class SecurityUtils {
             throw new IllegalStateException("로그인한 상태가 아닙니다.");
         }
     }
+
+    private static final String ALGORITHM = "SHA-256";
+    private static final String salt = "https://github.com/CSeungJoo";
+
+    public static String hashing(String valueToEnc) throws NoSuchAlgorithmException {
+        String saltedVal = valueToEnc + salt;
+        MessageDigest digest = MessageDigest.getInstance(ALGORITHM);
+        byte[] hash = digest.digest(saltedVal.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(hash);
+    }
+
 }
