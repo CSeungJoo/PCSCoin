b8996796b113283b78b4b6aeea15981ad7c901ed
CSeungJoo <seungjoo13579@gmail.com>
2024-05-20 18:53:28 +0900

    메일 서비스 추가 및 비동기 처리 어노테이션 추가

--- src/main/java/kr/pah/pcs/pcscoin/PcsCoinApplication.java
+++ src/main/java/kr/pah/pcs/pcscoin/PcsCoinApplication.java
@@ -2,8 +2,10 @@ package kr.pah.pcs.pcscoin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PcsCoinApplication {
 
     public static void main(String[] args) {
--- src/main/java/kr/pah/pcs/pcscoin/global/service/MailService.java
+++ src/main/java/kr/pah/pcs/pcscoin/global/service/MailService.java
@@ -2,6 +2,7 @@ package kr.pah.pcs.pcscoin.global.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -13,9 +14,13 @@ public class MailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${spring.mail.username}")
+    private String fromEmail;
+
     @Async
     public void sendEmail(String recipientAddress, String subject, String text) {
         SimpleMailMessage email = new SimpleMailMessage();
+        email.setFrom(fromEmail);
         email.setTo(recipientAddress);
         email.setSubject(subject);
         email.setText(text);
